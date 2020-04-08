c77c29dc9a7bcf4b140bf90948bc9eb60967d379
mihailgaberov <mihail.gaberov@gmail.com>
2020-04-08 08:14:32 +0300

    :sparkles: Updated dummy data to facilitate booleans.

--- server/db.py
+++ server/db.py
@@ -2,33 +2,12 @@ from pymongo import MongoClient
 from flask import jsonify
 import uuid
 
-BOOKS = [
-    {
-        'id': uuid.uuid4().hex,
-        'title': 'On the Road',
-        'author': 'Jack Kerouac',
-        'read': True
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'title': 'Harry Potter and the Philosopher\'s Stone',
-        'author': 'J. K. Rowling',
-        'read': False
-    },
-    {
-         'id': uuid.uuid4().hex,
-        'title': 'Green Eggs and Ham',
-        'author': 'Dr. Seuss',
-        'read': True
-    }
-]
-
 class DB:
     def __init__(self):
         print('[*] Initializing DB object')
         client = MongoClient('mongodb://localhost:27017/')
         self.db = client.libreria
-        
+
     def get_all(self):
         print('[*] Getting all books from DB')
         resp = {'status': 'success'}
@@ -39,6 +18,7 @@ class DB:
           book = {}
           book['title'] = doc['title']
           book['author'] = doc['author']
+          book['read'] = doc['read']
           books.append(book)
 
         #resp['books'] = BOOKS
@@ -46,7 +26,7 @@ class DB:
         return jsonify(resp)
         # cursor = self.db.books.find({})
         # books = []
-        
+
         # for doc in cursor:
         #     print('[*] Adding a book to the array to be returned: ', doc['title'])
         #     books.append(doc)
